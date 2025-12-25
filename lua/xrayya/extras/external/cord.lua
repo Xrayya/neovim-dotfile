60af8c639cefb47f8f966ad330e0e603df5449d5
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 20:15:34 +0700

    extras: add external which has cord and wakatime

---
+++ lua/xrayya/extras/external/cord.lua
@@ -0,0 +1,65 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "vyfor/cord.nvim",
+  event = "VeryLazy",
+  build = ":Cord update",
+  ---@module "cord.nvim"
+  ---@type CordConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    text = {
+      viewing = function(opts)
+        return "Viewing " .. opts.filename
+      end,
+      editing = function(opts)
+        local text = "Editing " .. opts.filename
+        if vim.bo.modified then
+          text = text .. "[+]"
+        end
+        return text
+      end,
+      file_browser = function(opts)
+        return "Browsing files using " .. opts.tooltip
+      end,
+      plugin_manager = function(opts)
+        return "Managing plugins using " .. opts.tooltip
+      end,
+      lsp = function(opts)
+        return "Configuring LSP using " .. opts.tooltip
+      end,
+      docs = function(opts)
+        return "Reading " .. opts.tooltip
+      end,
+      vcs = function(opts)
+        return "Committing changes using " .. opts.tooltip
+      end,
+      workspace = function(opts)
+        local hour = tonumber(os.date("%H"))
+        local status = hour >= 22 and "🌙 Late night coding"
+            or hour >= 18 and "🌆 Evening session"
+            or hour >= 12 and "☀️ Afternoon coding"
+            or hour >= 5 and "🌅 Morning productivity"
+            or "🌙 Midnight hacking"
+
+        return string.format("%s: %s", status, opts.workspace)
+      end,
+      dashboard = "Home",
+    },
+    buttons = {
+      {
+        label = function(opts)
+          return opts.repo_url and "View Repository" or "Website"
+        end,
+        url = function(opts)
+          return opts.repo_url or "https://example.com"
+        end,
+      },
+    },
+    idle = {
+      details = function(opts)
+        return string.format("Taking a break from %s", opts.workspace)
+      end,
+    },
+  },
+}
