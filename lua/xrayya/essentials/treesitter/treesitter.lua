c5ab5c877c08d321ffd6b72e76d1ec6cd5c96e2f
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 20:14:25 +0700

    essentials: move treesitter to treesitter folder

--- lua/xrayya/essentials/treesitter/treesitter.lua
+++ lua/xrayya/essentials/treesitter/treesitter.lua
@@ -1,14 +1,16 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
     config = function(_, opts)
       require("nvim-treesitter.install").compilers = { "gcc", "clang" }
 
       require("nvim-treesitter.configs").setup(opts)
 
       vim.o.foldmethod = "expr"
-      vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+      vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
       vim.o.foldlevelstart = 99
-    end
-  }
+    end,
+  },
 }
