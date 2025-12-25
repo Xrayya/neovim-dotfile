cd06643468a28c376b2fc044c71cfdcde759ef44
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 22:19:54 +0700

    core.autocompletion: set config for type snippet ft extension

--- lua/xrayya/core/modules/autocompletion.lua
+++ lua/xrayya/core/modules/autocompletion.lua
@@ -4,6 +4,30 @@ return {
   {
     "L3MON4D3/LuaSnip",
     build = "make install_jsregexp",
+    config = function()
+      local luasnip = require("luasnip")
+
+      require("luasnip.loaders.from_vscode").lazy_load({
+        exclude = { "java" },
+      })
+
+      luasnip.filetype_extend("php", { "html" })
+      luasnip.filetype_extend("blade", { "html" })
+      luasnip.filetype_extend("javascriptreact", { "html", "jsdoc" })
+      luasnip.filetype_extend("typescriptreact", { "html", "tsdoc" })
+      luasnip.filetype_extend("javascript", { "jsdoc" })
+      luasnip.filetype_extend("typescript", { "tsdoc" })
+      luasnip.filetype_extend("lua", { "luadoc" })
+      luasnip.filetype_extend("python", { "pydoc" })
+      luasnip.filetype_extend("rust", { "rustdoc" })
+      luasnip.filetype_extend("cs", { "csharpdoc" })
+      luasnip.filetype_extend("c", { "cdoc" })
+      luasnip.filetype_extend("cpp", { "cppdoc" })
+      luasnip.filetype_extend("php", { "phpdoc" })
+      luasnip.filetype_extend("kotlin", { "kdoc" })
+      luasnip.filetype_extend("ruby", { "rdoc" })
+      luasnip.filetype_extend("sh", { "shelldoc" })
+    end,
     keys = {
       {
         "<M-l>",
