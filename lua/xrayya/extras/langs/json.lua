9c38626be808b638dd75278894e6a292b2a6b917
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 21:58:58 +0700

    extras.langs: add json

---
+++ lua/xrayya/extras/langs/json.lua
@@ -0,0 +1,41 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers["jsonls"] = {
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    ---@module "nvim-treesitter"
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      ensure_installed = { "json", "jsonc", "json5" },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      opts.ensure_installed =
+          require("xrayya.utils.lsp").check_ensure_installed({ "jsonls" }, opts.ensure_installed)
+    end,
+  },
+}
