683c2c28dc094b8e461ed1c84495608143d9a6ad
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 21:48:17 +0700

    lsp: ensure setup all installed servers

--- lua/xrayya/core/modules/lsp.lua
+++ lua/xrayya/core/modules/lsp.lua
@@ -4,7 +4,7 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     dependencies = { "mason-org/mason.nvim" },
-    opts = {}
+    opts = {},
   },
   {
     "neovim/nvim-lspconfig",
@@ -13,15 +13,22 @@ return {
     },
     config = function(_, opts)
       local servers = opts.servers or {}
+
       for server_name, server_opts in pairs(servers) do
         local cmp_nvim_lsp_cap = {
           capabilities = require("cmp_nvim_lsp").default_capabilities(),
         }
 
-        server_opts = vim.tbl_deep_extend("force", server_opts, cmp_nvim_lsp_cap);
+        server_opts = vim.tbl_deep_extend("force", server_opts, cmp_nvim_lsp_cap)
 
         vim.lsp.config(server_name, server_opts)
         vim.lsp.enable(server_name)
+      end
+
+      local installed_servers = require("mason-lspconfig").get_installed_servers()
+
+      for _, server in pairs(installed_servers) do
+        vim.lsp.enable(server)
       end
     end,
   },
