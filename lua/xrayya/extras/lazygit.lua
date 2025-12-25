ecb5dcd6c3cf95746848c40b87b216e40ae143fc
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 20:18:11 +0700

    extras: add lazygit

---
+++ lua/xrayya/extras/lazygit.lua
@@ -0,0 +1,11 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.g.lazygit_floating_window_scaling_factor = 0.8
+    end,
+  },
+}
