cf74d92c31ed4592685a142b3feab125f3c56b22
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 15:28:12 +0700

    core: add statusline

---
+++ lua/xrayya/core/modules/statusline/init.lua
@@ -0,0 +1,49 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local components = require("xrayya.core.modules.statusline.components")
+    local custom_extension = require("xrayya.core.modules.statusline.custom_extensions")
+
+    require("lualine").setup({
+      options = {
+        disabled_filetypes = {
+          "alpha",
+        },
+      },
+      sections = {
+        lualine_a = { components.mode },
+        lualine_b = { components.filename },
+        lualine_c = { components.branch, components.diff },
+        lualine_x = { components.diagnostics, components.lsp },
+        lualine_y = { components.encoding, components.fileformat, components.filetype },
+        lualine_z = { components.location, components.progress },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      extensions = {
+        "quickfix",
+        "nvim-dap-ui",
+        "nvim-tree",
+        custom_extension.color_picker,
+        custom_extension.lspinfo,
+        custom_extension.null_ls_info,
+        custom_extension.sagaoutline,
+        custom_extension.lazy,
+        custom_extension.mason,
+        custom_extension.telescope,
+        custom_extension.trouble,
+        custom_extension.lazygit,
+      },
+    })
+  end,
+}
---
+++ lua/xrayya/utils/color.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+---@param decimal_value integer
+---@return string|nil
+M.convert_decimal_to_hex = function(decimal_value)
+  if decimal_value == nil then
+    return nil
+  end
+  return string.format("#%x", decimal_value)
+end
+
+return M
