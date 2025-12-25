d449fd2c2e00392e863974b43406a4a31d29990e
Xrayya <nur.alkhiyar@gmail.com>
2025-12-25 18:40:21 +0700

    extas.essentials: add comment and navigation

---
+++ lua/xrayya/extras/essentials/navigation.lua
@@ -0,0 +1,14 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "nacro90/numb.nvim",
+    event = "VeryLazy",
+    opts = {
+      show_numbers = true,
+      show_cursorline = true,
+      number_only = true,
+      centered_peeking = true,
+    },
+  },
+}
