12a9081bb4d742db961601367b7791ec756b4119
Xrayya <nur.alkhiyar@gmail.com>
2025-12-26 18:15:22 +0700

    essentials: setup debugger base

---
+++ lua/xrayya/essentials/debugger/init.lua
@@ -0,0 +1,77 @@
+local dapui_layouts = require("xrayya.essentials.debugger.layout")
+
+---@module "lazy"
+---@type LazySpec
+return {
+  { "mfussenegger/nvim-dap" },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      opts = {},
+    },
+    opts = {},
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    ---@module "dapui"
+    ---@type dapui.Config
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      layouts = dapui_layouts.running_console,
+    },
+    config = function(_, opts)
+      local dapui = require("dapui")
+      local dap = require("dap")
+
+      dapui.setup(opts)
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+
+      local function setup_dap_with_layout(layout)
+        local dapui_opts = vim.tbl_deep_extend("force", opts, {
+          layouts = layout,
+        })
+
+        dapui.setup(dapui_opts)
+      end
+
+      setup_dap_with_layout(dapui_layouts.running_console)
+
+      vim.api.nvim_create_user_command("DapSetupRunningConsole", function(_)
+        setup_dap_with_layout(dapui_layouts.running_console)
+      end, {})
+      vim.api.nvim_create_user_command("DapSetupRunning", function(_)
+        setup_dap_with_layout(dapui_layouts.running)
+      end, {})
+      vim.api.nvim_create_user_command("DapSetupDebugging", function(_)
+        setup_dap_with_layout(dapui_layouts.debugging)
+      end, {})
+      vim.api.nvim_create_user_command("DapSetupDebuggingConsole", function(_)
+        setup_dap_with_layout(dapui_layouts.debugging_console)
+      end, {})
+    end,
+  },
+
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ---@module "nvim-dap-virtual-text"
+    ---@type nvim_dap_virtual_text_options
+    opts = {
+      only_first_definition = false,
+      all_references = true,
+    },
+  },
+}
---
+++ lua/xrayya/essentials/debugger/layout.lua
@@ -0,0 +1,105 @@
+---@module "dapui"
+---@type table<string, dapui.Config.layout[]>
+local M = {}
+M.running = {
+  {
+    elements = {
+      {
+        id = "repl",
+        size = 1,
+      },
+    },
+    position = "bottom",
+    size = 10,
+  },
+}
+M.running_console = {
+  {
+    elements = {
+      {
+        id = "repl",
+        size = 0.2,
+      },
+      {
+        id = "console",
+        size = 0.8,
+      },
+    },
+    position = "bottom",
+    size = 0.45,
+  },
+}
+M.debugging_console = {
+  {
+    elements = {
+      {
+        id = "scopes",
+        size = 0.25,
+      },
+      {
+        id = "breakpoints",
+        size = 0.25,
+      },
+      {
+        id = "stacks",
+        size = 0.25,
+      },
+      {
+        id = "watches",
+        size = 0.25,
+      },
+    },
+    position = "left",
+    size = 40,
+  },
+  {
+    elements = {
+      {
+        id = "repl",
+        size = 0.5,
+      },
+      {
+        id = "console",
+        size = 0.5,
+      },
+    },
+    position = "bottom",
+    size = 0.45,
+  },
+}
+M.debugging = {
+  {
+    elements = {
+      {
+        id = "scopes",
+        size = 0.25,
+      },
+      {
+        id = "breakpoints",
+        size = 0.25,
+      },
+      {
+        id = "stacks",
+        size = 0.25,
+      },
+      {
+        id = "watches",
+        size = 0.25,
+      },
+    },
+    position = "left",
+    size = 40,
+  },
+  {
+    elements = {
+      {
+        id = "repl",
+        size = 1,
+      },
+    },
+    position = "bottom",
+    size = 10,
+  },
+}
+
+return M
