86d9351c5e1374c21e8323d149e48408b07e04d6
Xrayya <nur.alkhiyar@gmail.com>
2025-12-26 20:44:58 +0700

    extras.langs: extend compile user command and import codelldb config

--- lua/xrayya/extras/langs/c-cpp.lua
+++ lua/xrayya/extras/langs/c-cpp.lua
@@ -1,28 +1,34 @@
-vim.api.nvim_create_user_command("CppCompile", function()
-  local input_file = vim.fn.expand("%:p")
-  local output_file = vim.fn.expand("%:p:r")
+---@param compiler string
+function create_compile_command(compiler)
+  vim.api.nvim_create_user_command("CppCompile", function()
+    local input_file = vim.fn.expand("%:p")
+    local output_file = vim.fn.expand("%:p:r")
 
-  if vim.fn.has("win32") > 0 then
-    output_file = output_file .. ".exe"
-  else
-    output_file = output_file .. ".out"
-  end
-
-  ---@diagnostic disable-next-line: missing-fields
-  vim.uv.spawn("g++", {
-    args = { "-fdiagnostics-color=always", "-Wall", "-g", input_file, "-o", output_file },
-  }, function(code, _)
-    if code == 0 then
-      vim.notify("Compiling done", vim.log.levels.INFO, {
-        title = "C++ Compilation",
-      })
+    if vim.fn.has("win32") > 0 then
+      output_file = output_file .. ".exe"
     else
-      vim.notify("Compiling failed", vim.log.levels.ERROR, {
-        title = "C++ Compilation",
-      })
+      output_file = output_file .. ".out"
     end
-  end)
-end, {})
+
+    ---@diagnostic disable-next-line: missing-fields
+    vim.uv.spawn(compiler, {
+      args = { "-fdiagnostics-color=always", "-Wall", "-g", input_file, "-o", output_file },
+    }, function(code, _)
+      if code == 0 then
+        vim.notify("Compiling done", vim.log.levels.INFO, {
+          title = "C++ Compilation",
+        })
+      else
+        vim.notify("Compiling failed", vim.log.levels.ERROR, {
+          title = "C++ Compilation",
+        })
+      end
+    end)
+  end, {})
+end
+
+create_compile_command("g++")
+create_compile_command("gcc")
 
 local query_driver, resource_dir
 if vim.fn.has("win32") > 0 then
@@ -48,6 +54,7 @@ end
 ---@module "lazy"
 ---@type LazySpec
 return {
+
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
@@ -74,4 +81,5 @@ return {
       opts.ensure_installed = require("xrayya.utils.lsp").check_ensure_installed({ "clangd" }, opts.ensure_installed)
     end,
   },
+  { import = "xrayya.extras.debugger.codelldb" },
 }
