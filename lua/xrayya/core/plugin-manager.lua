d4e7ca2e88a5f39aeb216acf0255b71de29f383b
Xrayya <nur.alkhiyar@gmail.com>
2025-12-26 07:38:24 +0700

    core.plugin-manager: update bootstrap to immediately create `extra-config`

--- lua/xrayya/core/plugin-manager.lua
+++ lua/xrayya/core/plugin-manager.lua
@@ -6,7 +6,7 @@ if not vim.uv.fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -14,6 +14,61 @@ if not vim.uv.fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- bootstrap extra-config
+local extra_config_path = vim.fn.stdpath("config") .. "/lua/extra-config"
+if not vim.uv.fs_stat(extra_config_path) then
+  local out = vim.system({ "mkdir", "-p", extra_config_path })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone create `extra-config` folder:\n", "ErrorMsg" },
+      { out,                                               "WarningMsg" },
+      {
+        "You need to create `/lua/extra-config` folder manually",
+        "InfoMsg",
+      },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+if not vim.uv.fs_stat(extra_config_path .. "/init.lua") then
+  local out = vim.system({ "touch", extra_config_path .. "/init.lua" })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone create `/lua/extra-config/init.lua`:\n", "ErrorMsg" },
+      { out,                                                      "WarningMsg" },
+      {
+        "You need to create `/lua/extra-config/init.lua` file manually",
+        "InfoMsg",
+      },
+      {
+        "See `/lua/core/plugin-manager.lua` for the detail configuration",
+        "InfoMsg",
+      },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+
+  local content = string.format(
+    "%s\n\n%s\n%s\n%s\n%s\n%s\n%s",
+    "-- Other configuration that you need",
+    '---@module "lazy"',
+    "---@type LazySpec",
+    "return {",
+    '  -- { import = "xrayya.essentials" },',
+    '  -- { import = "xrayya.essentials.treesitter" },',
+    "}"
+  )
+
+  local file = assert(io.open(extra_config_path .. "/init.lua", "w"))
+  file:write(content)
+  file:close()
+end
 
 require("lazy").setup({
   spec = {
@@ -26,11 +81,12 @@ require("lazy").setup({
       priority = 1000,
       ---@module "tokyonight"
       ---@type tokyonight.Config
+      ---@diagnostic disable-next-line: missing-fields
       opts = {
         style = "night",
         light_style = "day",
         transparent = true,
-        on_highlights = function(highlights, colors)
+        on_highlights = function(highlights, _)
           highlights.LineNr.fg = highlights.LspInlayHint.fg
           highlights.LineNrAbove.fg = highlights.LspInlayHint.fg
           highlights.LineNrBelow.fg = highlights.LspInlayHint.fg
