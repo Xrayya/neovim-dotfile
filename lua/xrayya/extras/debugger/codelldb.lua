bab31a3219de14ffa130c791bb6f4658cde7a66b
Xrayya <nur.alkhiyar@gmail.com>
2025-12-27 10:57:15 +0700

    extras.{debugger.codelldb, langs.c-cpp}: remove compile and debug config since overseer is here right now

--- lua/xrayya/extras/debugger/codelldb.lua
+++ lua/xrayya/extras/debugger/codelldb.lua
@@ -7,43 +7,6 @@ return {
     ---@type MasonNvimDapSettings
     opts = {
       ensure_installed = { "codelldb" },
-      handlers = {
-        codelldb = function(config)
-          local extended_configurations = {
-            {
-              name = "LLDB: Launch current file",
-              type = "codelldb",
-              request = "launch",
-              program = function()
-                return vim.fn.expand("%:p:r") .. (vim.fn.has("win32") > 0 and ".exe" or ".out")
-              end,
-              cwd = "${workspaceFolder}",
-              stopOnEntry = false,
-              args = {},
-              console = "integratedTerminal",
-            },
-            {
-              name = "LLDB: Launch current file (args)",
-              type = "codelldb",
-              request = "launch",
-              program = function()
-                return vim.fn.expand("%:p:r") .. (vim.fn.has("win32") > 0 and ".exe" or ".out")
-              end,
-              cwd = "${workspaceFolder}",
-              stopOnEntry = false,
-              args = function()
-                return vim.split(vim.fn.input("Args: "), " +", { trimempty = true })
-              end,
-              console = "integratedTerminal",
-            },
-          }
-
-          config.configurations = config.configurations or {}
-          vim.list_extend(config.configurations, extended_configurations)
-
-          require("mason-nvim-dap").default_setup(config)
-        end,
-      },
     },
   },
 }
--- lua/xrayya/extras/langs/c-cpp.lua
+++ lua/xrayya/extras/langs/c-cpp.lua
@@ -1,35 +1,3 @@
----@param compiler string
-function create_compile_command(compiler)
-  vim.api.nvim_create_user_command("CppCompile", function()
-    local input_file = vim.fn.expand("%:p")
-    local output_file = vim.fn.expand("%:p:r")
-
-    if vim.fn.has("win32") > 0 then
-      output_file = output_file .. ".exe"
-    else
-      output_file = output_file .. ".out"
-    end
-
-    ---@diagnostic disable-next-line: missing-fields
-    vim.uv.spawn(compiler, {
-      args = { "-fdiagnostics-color=always", "-Wall", "-g", input_file, "-o", output_file },
-    }, function(code, _)
-      if code == 0 then
-        vim.notify("Compiling done", vim.log.levels.INFO, {
-          title = "C++ Compilation",
-        })
-      else
-        vim.notify("Compiling failed", vim.log.levels.ERROR, {
-          title = "C++ Compilation",
-        })
-      end
-    end)
-  end, {})
-end
-
-create_compile_command("g++")
-create_compile_command("gcc")
-
 local query_driver, resource_dir
 if vim.fn.has("win32") > 0 then
   query_driver = os.getenv("CLANGD_QUERY_DRIVER_PATH")       -- e.g "C:\\Users\\Hary\\scoop\\apps\\msys2\\current\\mingw64\\bin\\g++.exe"
