1fc9ef0a7de94aa607777e86f73fefc56d105ec6
Xrayya <nur.alkhiyar@gmail.com>
2025-12-28 17:43:49 +0700

    essentials.treesitter.textsubjects(fix): use fork that adopt new nvim-treesitter architecture
    probably just temporary

--- lua/xrayya/essentials/treesitter/textsubjects.lua
+++ lua/xrayya/essentials/treesitter/textsubjects.lua
@@ -1,7 +1,8 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  "RRethy/nvim-treesitter-textsubjects",
+  "kevintraver/nvim-treesitter-textsubjects",
+  branch = "nvim-treesitter-main-migration",
   opts = {},
   config = function(_, opts)
     require("nvim-treesitter-textsubjects").configure(opts)
