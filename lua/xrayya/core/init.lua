4d83016b9b7f6d6fd294f12c1ae2381a0f25e5b5
Xrayya <nur.alkhiyar@gmail.com>
2025-12-29 09:41:54 +0700

    core: activate core.auto

--- lua/xrayya/core/init.lua
+++ lua/xrayya/core/init.lua
@@ -5,5 +5,6 @@ require("xrayya.core.options")
 require("xrayya.core.keymaps")
 require("xrayya.core.plugin-manager")
 require("xrayya.core.lsp")
+require("xrayya.core.auto")
 
 vim.cmd.colorscheme("tokyonight")
