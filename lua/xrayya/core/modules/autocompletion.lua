131ea198dbe52bfc6c4fa037f98df6711074ba62
Xrayya <nur.alkhiyar@gmail.com>
2025-12-29 12:24:56 +0700

    core.autocompletion: remove friendly snippet from dependencies for modularity

--- lua/xrayya/core/modules/autocompletion.lua
+++ lua/xrayya/core/modules/autocompletion.lua
@@ -3,7 +3,6 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
     -- use a release tag to download pre-built binaries
     version = "1.*",
     ---@module 'blink.cmp'
