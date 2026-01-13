c1bc9a35de8191c005b9e54752dd652b21c4383f
Xrayya <nur.alkhiyar@gmail.com>
2026-01-13 09:18:51 +0700

    utils.buf: add opts for custom measurement on `is_larg_buf` function

--- lua/xrayya/utils/buf.lua
+++ lua/xrayya/utils/buf.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 ---@param buffer integer
+---@param opts? {max_size?: integer, max_line_count?: integer}
 ---@return boolean
-function M.is_large_buf(buffer)
-  local max_size = 100 * 1024 -- 100 KB
-  local max_line_count = 15000
+function M.is_large_buf(buffer, opts)
+  local max_size = opts and opts.max_size or (100 * 1024) -- 100 KB
+  local max_line_count = opts and opts.max_line_count or 15000
   local size = vim.fn.getfsize(vim.api.nvim_buf_get_name(buffer))
   local line_count = vim.api.nvim_buf_line_count(buffer)
   return size >= max_size or line_count >= max_line_count
