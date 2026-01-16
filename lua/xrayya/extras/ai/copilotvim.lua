300cdea684696ad1b08d8bd4871ca630015d6cb8
Xrayya <nur.alkhiyar@gmail.com>
2026-01-16 21:25:22 +0700

    extras.ai: add cipilot.vim and codecompanion.nvim

---
+++ lua/xrayya/extras/ai/copilotvim.lua
@@ -0,0 +1,14 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+}
