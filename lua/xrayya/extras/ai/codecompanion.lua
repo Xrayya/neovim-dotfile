b52954f320a2c15008087916ac9a56129481532f
Xrayya <nur.alkhiyar@gmail.com>
2026-02-08 07:31:10 +0700

    extras.ai.codecompanion: remove version and refactor dependencies

--- lua/xrayya/extras/ai/codecompanion.lua
+++ lua/xrayya/extras/ai/codecompanion.lua
@@ -3,11 +3,8 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    version = "^18.0.0",
     opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
   },
+  "nvim-lua/plenary.nvim",
+  "nvim-treesitter/nvim-treesitter",
 }
