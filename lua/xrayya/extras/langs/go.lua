a5fa509671ee3208c1d1290f714b3bec2392b20a
Xrayya <nur.alkhiyar@gmail.com>
2026-02-08 07:41:37 +0700

    extras.langs: add go

---
+++ lua/xrayya/extras/langs/go.lua
@@ -0,0 +1,45 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts = require("xrayya.utils.treesitter").extend(opts, {
+        ensure_installed = { "go" },
+        ensure_highlight = { "go" },
+        ensure_indent = { "go" },
+        ensure_fold = { "go" },
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      opts.ensure_installed = require("xrayya.utils.lsp").check_ensure_installed({ "gopls" }, opts.ensure_installed)
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@param opts Xray.lspconfigOpts
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers["gopls"] = {
+        settings = {
+          gopls = {
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+          },
+        }
+      }
+    end,
+  },
+}
