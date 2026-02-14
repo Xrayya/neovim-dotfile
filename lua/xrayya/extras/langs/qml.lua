4cc2d3c16dac123d4c08577b77e75d54129867bc
Xrayya <nur.alkhiyar@gmail.com>
2026-02-14 19:01:01 +0700

    extras.langs: add qml

---
+++ lua/xrayya/extras/langs/qml.lua
@@ -0,0 +1,30 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts = require("xrayya.utils.treesitter").extend(opts, {
+        ensure_installed = { "qmljs" },
+        ensure_highlight = { "qmljs" },
+        ensure_fold = { "qmljs" },
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      opts.ensure_installed = require("xrayya.utils.lsp").check_ensure_installed({ "qmlls" }, opts.ensure_installed)
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@param opts Xray.lspconfigOpts
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers["qmlls"] = {}
+    end,
+  },
+}
