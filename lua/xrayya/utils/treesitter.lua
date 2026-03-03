7d67253a16ef44398f6511ff1472eb1484a4fd92
Xrayya <nur.alkhiyar@gmail.com>
2026-03-04 06:46:22 +0700

    utils.treesitter(perf): remove redundant code

--- lua/xrayya/utils/treesitter.lua
+++ lua/xrayya/utils/treesitter.lua
@@ -6,11 +6,6 @@ local M = {}
 ---@param opts Xray.treesitterOpts
 ---@param addition_opts Xray.treesitterOpts
 M.extend = function(opts, addition_opts)
-  opts.ensure_installed = opts.ensure_installed or {}
-  opts.ensure_highlight = opts.ensure_highlight or {}
-  opts.ensure_indent = opts.ensure_indent or {}
-  opts.ensure_fold = opts.ensure_fold or {}
-
   opts.ensure_installed = adt.merge_set(opts.ensure_installed or {}, addition_opts.ensure_installed or {})
   opts.ensure_highlight = adt.merge_set(opts.ensure_highlight or {}, addition_opts.ensure_highlight or {})
   opts.ensure_indent = adt.merge_set(opts.ensure_indent or {}, addition_opts.ensure_indent or {})
