6b7c4cd195403806d247e54f13ccc44ce220f56e
Xrayya <nur.alkhiyar@gmail.com>
2026-04-23 08:34:39 +0700

    extras.langs: add js-ts-tsgo

---
+++ lua/xrayya/extras/langs/js-ts-tsgo.lua
@@ -0,0 +1,85 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "saghen/blink.cmp",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      sources = {
+        providers = {
+          snippets = {
+            opts = {
+              extended_filetypes = {
+                javascriptreact = { "html", "jsdoc" },
+                typescriptreact = { "html", "tsdoc" },
+                javascript = { "jsdoc" },
+                typescript = { "tsdoc" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts = require("xrayya.utils.treesitter").extend(opts, {
+        ensure_installed = { "javascript", "jsx", "jsdoc", "typescript", "tsx", "comment" },
+        ensure_highlight = { "javascript", "jsx", "jsdoc", "typescript", "tsx", "comment" },
+        ensure_indent = { "javascript", "typescript", "tsx" },
+        ensure_fold = { "javascript", "typescript", "tsx" },
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      opts.ensure_installed = require("xrayya.utils.lsp").check_ensure_installed({ "tsgo" }, opts.ensure_installed)
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@param opts Xray.lspconfigOpts
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      ---@type vim.lsp.Config
+      opts.servers["tsgo"] = {
+        init_options = {
+          preferences = {
+            importModuleSpecifierPreference = "shortest",
+          },
+        },
+        ---@module "lspconfig"
+        ---@type lspconfig.settings.ts_ls
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = false,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = false,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = true,
+            },
+          },
+        },
+      }
+    end,
+  },
+}
