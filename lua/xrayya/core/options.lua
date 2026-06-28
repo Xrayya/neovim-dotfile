a935f9a8aeb82b0fe884a4d460dc5ec2b5a34224
Xrayya <nur.alkhiyar@gmail.com>
2026-06-28 23:36:00 +0700

    Revert "core.options: built custom status column"
    This reverts commit 9bcc53399bdc1e5fc7e1f67bb4f99e4ebcbda854.

--- lua/xrayya/core/options.lua
+++ lua/xrayya/core/options.lua
@@ -47,82 +47,11 @@ set.shortmess:append("c")
 set.termguicolors = true
 set.foldlevel = 99
 set.foldlevelstart = 99
-
-local statuscol_cache = {}
-
--- Clear the cache when text changes or when switching buffers
-vim.api.nvim_create_autocmd({
-  "DiagnosticChanged", -- Wipes cache instantly when LSP diagnostics update
-  "User",
-  "TextChanged",
-  "TextChangedI",
-  "BufEnter",
-}, {
-  group = vim.api.nvim_create_augroup("StatusColCacheClear", { clear = true }),
-  callback = function()
-    statuscol_cache = {}
-  end,
-})
-
-_G.my_statuscolumn = function()
-  if vim.v.virtnum ~= 0 then
-    return "%=" -- Prevent duplicate signs on wrapped line screens
-  end
-
-  local lnum = vim.v.lnum
-  local bufnr = vim.api.nvim_get_current_buf()
-
-  local cache_key = string.format("%d_%d", bufnr, lnum)
-  if statuscol_cache[cache_key] then
-    return statuscol_cache[cache_key]
-  end
-
-  local left_sign = ""
-  local lsp_sign = ""
-  local git_sign = ""
-
-  local extmarks = vim.api.nvim_buf_get_extmarks(bufnr, -1, { lnum - 1, 0 }, { lnum - 1, -1 }, { details = true })
-
-  for _, extmark in ipairs(extmarks) do
-    local details = extmark[4]
-
-    if details and details.sign_text then
-      local ns_id = extmark[2]
-      local ns_name = vim.api.nvim_get_namespaces()[ns_id] or ""
-      local hl_group = details.sign_hl_group or "Normal"
-
-      -- Format the character safely with its intended color highlight group
-      local formatted_sign = string.format("%%#%s#%s%%*", hl_group, details.sign_text)
-
-      if ns_name:find("gitsigns") or hl_group:find("GitSigns") then
-        git_sign = formatted_sign
-      elseif ns_name:find("diagnostic") or hl_group:find("Diagnostic") then
-        lsp_sign = formatted_sign
-      else
-        left_sign = formatted_sign
-      end
-    end
-  end
-
-  local left_pad = (left_sign ~= "") and left_sign or "  "
-  local lsp_pad = (lsp_sign ~= "") and lsp_sign or "  "
-  local git_pad = (git_sign ~= "") and git_sign or " "
-
-  local result = string.format("%s %%l %s%%=%s", left_pad, lsp_pad, git_pad)
-
-  statuscol_cache[cache_key] = result
-  return result
-end
-
-vim.opt.signcolumn = "no"
-vim.opt.statuscolumn = "%!v:lua.my_statuscolumn()"
-
 vim.opt.fillchars:append({
-  fold = "·",
+  fold = " ",
   foldopen = "",
   foldclose = "",
-  foldsep = "│",
-  foldinner = "│",
+  foldsep = " ",
 })
 
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
@@ -134,3 +63,4 @@ vim.filetype.add({
     [".*/%.vscode/launch%.json"] = "jsonc",
   },
 })
+
