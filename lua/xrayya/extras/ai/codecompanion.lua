458d5bc0bca153042e318241ea560270ccb48836
Xrayya <nur.alkhiyar@gmail.com>
2026-09-07 20:43:30 +0700

    extras.ai.codecompanion: use vertex ai

--- lua/xrayya/extras/ai/codecompanion.lua
+++ lua/xrayya/extras/ai/codecompanion.lua
@@ -3,7 +3,39 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    dependencies = {
+      "viespejo/cc-adapter-vertex-ai.nvim",
+    },
+    init = function()
+      -- Backward compatibility shim: recent versions of codecompanion.nvim moved
+      -- `codecompanion.utils.adapters` to `codecompanion.adapters.utils`.
+      -- cc-adapter-vertex-ai.nvim imports the old module path.
+      package.preload["codecompanion.utils.adapters"] = function()
+        return require("codecompanion.adapters.utils")
+      end
+    end,
+    opts = {
+      interactions = {
+        chat = {
+          adapter = "vertex_gemini",
+        },
+        inline = {
+          adapter = "vertex_gemini",
+        },
+      },
+      adapters = {
+        http = {
+          vertex_gemini = function()
+            return require("codecompanion.adapters").extend("vertex-gemini", {
+              env = {
+                project_id = "valid_id",
+                region = "global",
+              },
+            })
+          end,
+        },
+      },
+    },
   },
   "nvim-lua/plenary.nvim",
   "nvim-treesitter/nvim-treesitter",
